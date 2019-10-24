7ac46bae15d51f9833969052467842b5415eea8b
GioPietrangelo <gigio.pietrangelo@gmail.com>
2019-10-24 17:41:27 -0300

    Gogogogo

--- Lab2/Projects/s11_g01_lab2/src/s11_g01_lab2.c
+++ Lab2/Projects/s11_g01_lab2/src/s11_g01_lab2.c
@@ -43,6 +43,10 @@ double periodo = 0;
 int count=0;
 uint32_t  start = 0, end = 0, length = 0;
 
+// TEST
+const int timerLoadValue = 65534;
+const int timerPrescaleLoadValue = 255;
+
 extern void UARTStdioIntHandler(void);
 
 void UARTInit(void){
@@ -74,8 +78,7 @@ void TIMER0A_Handler(void)
   end = TimerValueGet(TIMER0_BASE, TIMER_B);
   length = end - start;
 
-  printf("TIMER A\n");
-  UARTprintf("LENGTH = %d |", length);
+  UARTprintf("LENGTH = %d ", length);
 }
 
 void TIMER0B_Handler(void)
@@ -85,8 +88,7 @@ void TIMER0B_Handler(void)
   end = TimerValueGet(TIMER0_BASE, TIMER_B);
   length = end - start;
 
-  printf("TIMER B\n");
-  UARTprintf("LENGTH = %d |", length);
+  UARTprintf("LENGTH = %d ", length);
 }
 
 void main(void){
@@ -96,48 +98,53 @@ void main(void){
   SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOL);
   while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOL));
   
+  // Habilita o TIMER 0.
   SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
 
-  // Habilita o GPIO Port L.
+  // Configura a função alternativa do GPIO Port L Pino 4.
   GPIOPinConfigure(GPIO_PL4_T0CCP0);
+  
+  // Habilita a função alternativa como TIMER para o GPIO Port L Pino 4.
   GPIOPinTypeTimer(GPIO_PORTL_BASE, GPIO_PIN_4);
+  
+  // Configura a função alternativa do GPIO Port L Pino 5.
   GPIOPinConfigure(GPIO_PL5_T0CCP1);
+  
+  // Habilita a função alternativa como TIMER para o GPIO Port L Pino 5.
   GPIOPinTypeTimer(GPIO_PORTL_BASE, GPIO_PIN_5);
-     
+  
+  // Habilita as interrupções para o processador, ou seja, permite ao processador responder a interrupções.
   IntMasterEnable();
+  
+  // Habilita interrupções para o TIMER 0A.
   IntEnable(INT_TIMER0A);
+  
+  // Habilita interrupções para o TIMER 0B.
   IntEnable(INT_TIMER0B);
 
-  //Configura o Timer 4 como sendo um Timer A e um B. O Timer A é configurado
-  //para contar bordas de forma crescente e definido para 16/24 bits (prescale).
+  // Configura o TIMER 0 como sendo um TIMER A e um B.
   TimerConfigure(TIMER0_BASE, (TIMER_CFG_SPLIT_PAIR | TIMER_CFG_A_CAP_TIME_UP | TIMER_CFG_B_CAP_TIME_UP));
   
-  //TimerPrescaleSet(TIMER0_BASE, TIMER_A, 255);
-  //TimerMatchSet(TIMER0_BASE, TIMER_A, 0);
+  //TimerLoadSet(TIMER0_BASE, TIMER_BOTH, 0x9C3F);
   
-  //Define o TIPO de evento que o Timer deve gerenciar. 
-  //Por tipo de evento se entende se é uma Borda de Subida, Descida ou ambas as bordas
+  // Define o TIPO de evento que o TIMER deve gerenciar. 
+  // Por tipo de evento se entende se é uma BORDA de SUBIDA, DESCIDA ou ambas as bordas
   TimerControlEvent(TIMER0_BASE, TIMER_A, TIMER_EVENT_POS_EDGE); // SUBIDA
   TimerControlEvent(TIMER0_BASE, TIMER_B, TIMER_EVENT_NEG_EDGE); // DESCIDA
-  
-  //Set the value that the timers count to (0x9C3F = 39999)
-  //CO2 sensor outputs 1khz pwm so with mcu at 40Mhz, timers should stay in sync with CO2 output.
-  TimerLoadSet(TIMER0_BASE, TIMER_BOTH, 0x9C3F);
-  //TimerLoadSet(TIMER0_BASE, TIMER_BOTH, timer0load);
-  
-  //Registra uma função para lidar com a interrupção do timer quando o Timer B atingir um evento de borda de descida.
+    
+  // Registra uma função para lidar com a interrupção do timer quando o TIMER B atingir um evento de borda de descida.
   TimerIntRegister(TIMER0_BASE, TIMER_A, TIMER0A_Handler);
   TimerIntRegister(TIMER0_BASE, TIMER_B, TIMER0B_Handler);
 
   // Makes sure the interrupt is cleared
-  //TimerIntClear(TIMER0_BASE, TIMER_CAPA_EVENT);
+  TimerIntClear(TIMER0_BASE, TIMER_CAPA_EVENT);
   TimerIntClear(TIMER0_BASE, TIMER_CAPB_EVENT);
   
   // Enable the indicated timer interrupt source.
-  //TimerIntEnable(TIMER0_BASE, TIMER_CAPA_EVENT);
+  TimerIntEnable(TIMER0_BASE, TIMER_CAPA_EVENT);
   TimerIntEnable(TIMER0_BASE, TIMER_CAPB_EVENT);
 
-  // Enable the timers.
+  // Habilita os timers.
   TimerEnable(TIMER0_BASE, TIMER_A);
   TimerEnable(TIMER0_BASE, TIMER_B);
 
